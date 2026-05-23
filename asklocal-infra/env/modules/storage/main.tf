dda769acea84ef0183a625224552d5c49abb11ed
phungh67 <phungh67@gmail.com>
2026-05-23 19:54:22 +0200

    feat: separate infra and code

--- asklocal-infra/env/modules/storage/main.tf
+++
@@ -1,9 +0,0 @@
-module "s3_bucket_storage" {
-  source = "github.com/phungh67/customized-terraform-modules//s3-storage"
-
-  bucket-prefix       = "${var.environment_code}${var.region_code}-storage-bucket"
-  service_type        = var.service_type
-  cors_specified      = var.cors_specified
-  policy_specified    = var.policy_specified
-  lifecycle_specified = var.life_cycle_specified
-}
--- asklocal-infra/env/modules/storage/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "s3_bucket_storage_id" {
-  value = module.s3_bucket_storage.s3_bucket_id
-}
