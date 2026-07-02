170a88c25f59289f0256077b4507d215f2d2b131
phungh67 <phungh67@gmail.com>
2026-07-02 18:30:31 +0200

    feat: storage handler with upload function

--- lokask-backend/backend/internal/handler/storage_handler.go
+++ lokask-backend/backend/internal/handler/storage_handler.go
@@ -3,7 +3,11 @@
 package handler
 
 import (
+	"asklocal/internal/helper"
 	"asklocal/internal/storage"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 type StorageHandler struct {
@@ -14,4 +18,34 @@ func NewStorageHandler(s storage.FileStorage) *StorageHandler {
 	return &StorageHandler{
 		Storage: s,
 	}
+}
+
+func (h *StorageHandler) UploadMedia(c *fiber.Ctx) (string, error) {
+	userIDStr := c.Locals("user_id").(string)
+
+	fileHeader, err := c.FormFile("avatar")
+	if err != nil {
+		return "", c.Status(400).JSON(fiber.Map{
+			"error": "No file was provided.",
+		})
+	}
+
+	key, err := helper.ValidateAndSecureFilename(fileHeader.Filename)
+	if err != nil {
+		log.Printf("[WARN][UPLOAD] Blocked upload: %v", err)
+		return "", c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Bad request.",
+		})
+	}
+
+	url, err := h.Storage.UploadFile(fileHeader, userIDStr, key)
+
+	if err != nil {
+		return url, nil
+	}
+
+	log.Printf("[ERROR][HANDLER] Upload failed due to: %v", err)
+	return "", c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+		"message": "Upload failed.",
+	})
 }
